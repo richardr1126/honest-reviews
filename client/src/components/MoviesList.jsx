625c7aca6078204deb163f74c88ac6a19d74db9f
richardr1126 <richardr1126@gmail.com>
2022-12-25 23:14:34 -0600

    fixing bugs

--- client/src/components/MoviesList.jsx
+++ client/src/components/MoviesList.jsx
@@ -7,8 +7,8 @@ import SearchBar from './SearchBar';
 function MoviesList(props) {
   const [listofMovies, setListOfMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  
-  
+
+
   useEffect(() => {
     Axios.get((process.env.NODE_ENV === 'production') ? '/api/movies/get' : 'http://localhost:3001/api/movies/get').then((response) => {
       //sort movies by latest review date
@@ -34,7 +34,7 @@ function MoviesList(props) {
               const movieDetailed = await Axios.get(`https://www.omdbapi.com/?apikey=306c054b&t=${movieUnformatted.Title}&type=movie`)
               if (movieDetailed.data.Response === 'True') {
                 console.log(movieDetailed.data);
-                
+
                 return {
                   _id: "",
                   title: movieDetailed.data.Title,
@@ -49,9 +49,10 @@ function MoviesList(props) {
                 return null;
               }
             });
-            
+
 
             const newMovies = await Promise.all(newMoviesPromises);
+            //check if any movies in the newMovies array have the same name, if they do filter them out
             setListOfMovies(listOfMovies => {
               const moviesToAdd = newMovies.filter(newMovie => !listOfMovies.some(movie => movie.title === newMovie.title));
               return [...listOfMovies, ...moviesToAdd];
@@ -64,25 +65,42 @@ function MoviesList(props) {
     }
   }, [searchTerm]);
 
+
   const filteredMovies = listofMovies.filter((movie) =>
     movie.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  filteredMovies.sort((a, b) => {
-    if (a.reviews.length > 0 && b.reviews.length > 0) {
-      return new Date(b.reviews[b.reviews.length - 1].date) - new Date(a.reviews[a.reviews.length - 1].date);
-    } else if (a.reviews.length > 0) {
-      return new Date(b.reviews[b.reviews.length - 1].date) - new Date(a.releaseDate);
-    } else if (b.reviews.length > 0) {
-      return new Date(b.releaseDate) - new Date(a.reviews[a.reviews.length - 1].date);
-    } else {
-      return new Date(b.releaseDate) - new Date(a.releaseDate);
+    && movie.posterImageUrl !== 'N/A'
+    && movie.plot !== 'N/A'
+    && movie.cast !== 'N/A'
+    && movie.genre !== 'N/A'
+  ).reduce((acc, current) => {
+    if (!acc.some((item) => item.title === current.title)) {
+      acc.push(current);
     }
-  });
+    return acc;
+  }, []);
+
+
+
+  try { //sort movies by latest review date
+    filteredMovies.sort((a, b) => {
+      if (a.reviews.length > 0 && b.reviews.length > 0) {
+        return new Date(b.reviews[b.reviews.length - 1].date) - new Date(a.reviews[a.reviews.length - 1].date);
+      } else if (a.reviews.length > 0) {
+        return new Date(b.reviews[b.reviews.length - 1].date) - new Date(a.releaseDate);
+      } else if (b.reviews.length > 0) {
+        return new Date(b.releaseDate) - new Date(a.reviews[a.reviews.length - 1].date);
+      } else {
+        return new Date(b.releaseDate) - new Date(a.releaseDate);
+      }
+    });
+  } catch (error) { //if no reviews are attached to movies
+    console.log("sorting error, no reviews attached to movies");
+  }
 
 
   return (
-    <div className='box container is-max-desktop' style={props.darkMode ? {backgroundColor: '#262626'} : {backgroundColor: 'white'}}>
-      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+    <div className='box container is-max-desktop' style={props.darkMode ? { backgroundColor: '#262626' } : { backgroundColor: 'white' }}>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       {filteredMovies.map((movie) => {
         return (
           <MovieCard darkMode={props.darkMode} movie={movie} setListOfMovies={setListOfMovies} listofMovies={listofMovies} />
